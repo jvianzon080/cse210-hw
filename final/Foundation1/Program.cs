1e07418fc820b729e7bbc3706a02bb81f90463dd
jvianzon080 <j.vianzon080@gmail.com>
2024-06-05 22:24:12 +0800

    Finished Foundation 1 and 2

--- final/Foundation1/Program.cs
+++ final/Foundation1/Program.cs
@@ -1,9 +1,73 @@
 using System;
+using System.Collections.Generic;
+
+public class Video
+{
+    public string Title { get; set; }
+    public string Author { get; set; }
+    public int LengthInSeconds { get; set; }
+    private List<Comment> Comments { get; set; } = new List<Comment>();
+
+    public void AddComment(string commenter, string text)
+    {
+        Comments.Add(new Comment { Commenter = commenter, Text = text });
+    }
+
+    public int GetNumberOfComments()
+    {
+        return Comments.Count;
+    }
+
+    public void DisplayInfo()
+    {
+        Console.WriteLine($"Title: {Title}");
+        Console.WriteLine($"Author: {Author}");
+        Console.WriteLine($"Length: {LengthInSeconds} seconds");
+        Console.WriteLine($"Number of comments: {GetNumberOfComments()}");
+        Console.WriteLine("Comments:");
+        foreach (var comment in Comments)
+        {
+            Console.WriteLine($"- {comment.Commenter}: {comment.Text}");
+        }
+        Console.WriteLine();
+    }
+}
+
+public class Comment
+{
+    public string Commenter { get; set; }
+    public string Text { get; set; }
+}
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Foundation1 World!");
+        var videos = new List<Video>();
+
+        var video1 = new Video
+        {
+            Title = "Van Life: Davao City",
+            Author = "Joseph Ezra Vianzon",
+            LengthInSeconds = 600
+        };
+        video1.AddComment("Armel", "Great video!");
+        video1.AddComment("Zaera", "I learned a lot, thanks!");
+
+        var video2 = new Video
+        {
+            Title = "Van Life: Surigao City",
+            Author = "Joseph Ezra Vianzon",
+            LengthInSeconds = 900
+        };
+        video2.AddComment("Ezra", "Very entertaining!");
+
+        videos.Add(video1);
+        videos.Add(video2);
+
+        foreach (var video in videos)
+        {
+            video.DisplayInfo();
+        }
     }
 }
--- final/Foundation2/Program.cs
+++ final/Foundation2/Program.cs
@@ -1,9 +1,126 @@
 using System;
+using System.Collections.Generic;
+
+public class Product
+{
+    public string Name { get; set; }
+    public int ProductId { get; set; }
+    public double Price { get; set; }
+    public int Quantity { get; set; }
+
+    public double GetTotalCost()
+    {
+        return Price * Quantity;
+    }
+}
+
+public class Address
+{
+    public string Street { get; set; }
+    public string City { get; set; }
+    public string State { get; set; }
+    public string Country { get; set; }
+
+    public bool IsInUSA()
+    {
+        return Country.ToLower() == "usa";
+    }
+
+    public string GetAddressAsString()
+    {
+        return $"{Street}, {City}, {State}, {Country}";
+    }
+}
+
+public class Customer
+{
+    public string Name { get; set; }
+    public Address Address { get; set; }
+
+    public bool IsInUSA()
+    {
+        return Address.IsInUSA();
+    }
+}
+
+public class Order
+{
+    public List<Product> Products { get; set; }
+    public Customer Customer { get; set; }
+
+    public double GetTotalPrice()
+    {
+        double totalPrice = 0;
+        foreach (var product in Products)
+        {
+            totalPrice += product.GetTotalCost();
+        }
+        if (Customer.IsInUSA())
+            totalPrice += 5;
+        else
+            totalPrice += 35;
+        return totalPrice;
+    }
+
+    public string GetPackingLabel()
+    {
+        string label = "Packing Label:\n";
+        foreach (var product in Products)
+        {
+            label += $"{product.Name}, Product ID: {product.ProductId}\n";
+        }
+        return label;
+    }
+
+    public string GetShippingLabel()
+    {
+        string label = "Shipping Label:\n";
+        label += $"Customer: {Customer.Name}\n";
+        label += $"Address: {Customer.Address.GetAddressAsString()}\n";
+        return label;
+    }
+}
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Foundation2 World!");
+        var product1 = new Product
+        {
+            Name = "Laptop",
+            ProductId = 1,
+            Price = 799.99,
+            Quantity = 2
+        };
+
+        var product2 = new Product
+        {
+            Name = "Mouse",
+            ProductId = 2,
+            Price = 7.99,
+            Quantity = 1
+        };
+
+        var customer = new Customer
+        {
+            Name = "Joseph Ezra Vianzon",
+            Address = new Address
+            {
+                Street = "123 Diamond St",
+                City = "San Diego",
+                State = "CA",
+                Country = "USA"
+            }
+        };
+
+        var order = new Order
+        {
+            Products = new List<Product> { product1, product2 },
+            Customer = customer
+        };
+
+        Console.WriteLine(order.GetPackingLabel());
+        Console.WriteLine(order.GetShippingLabel());
+        Console.WriteLine($"Total Price: ${order.GetTotalPrice():0.00}");
     }
 }
